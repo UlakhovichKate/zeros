53e83a2ad445984be45c07c0e6280dd7cf507a7a
Katsiaryna <36642433+UlakhovichKate@users.noreply.github.com>
2018-03-04 22:04:06 +0300

    result 4s

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function getZerosCount(number) {
   // your implementation
+  let count=0;
+  let n = 1;
+  for (let i=5; i<=number; i=i+5){
+    //if (i % 5 == 0){
+      count++;
+      n = i / 5;
+      while (n%5==0){
+        count++;
+        n = n / 5;
+      }
+    }
+  //}
+  return count;
+
 }
